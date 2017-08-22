587232ce40205fc2c4eb338e7349bcb314b96245
prateek211205361719 <prateek@ceptes.com>
2017-08-22 15:01:05 +0530

    f33f3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
     console.log('----------');
     var OAuth2 = google.auth.OAuth2;
-    var oauth2Client = new OAuth2('550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',  'NiUo_W8rZH3Wa4Ikkr3Ir8by', 'oauthcallback');
+    var oauth2Client = new OAuth2('550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',  'NiUo_W8rZH3Wa4Ikkr3Ir8by', 'https://shrouded-hamlet-68890.herokuapp.com/oauthcallback');
     var scopes = [
         'https://www.googleapis.com/auth/plus.me',
         'https://www.googleapis.com/auth/calendar'
