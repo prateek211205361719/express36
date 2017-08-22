0e91412bc07ee36504f6436f49b208177fdece06
prateek211205361719 <prateek@ceptes.com>
2017-08-22 11:12:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var express = require('express');
+var google = require('googleapis');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    var OAuth2 = google.auth.OAuth2;
+    var oauth2Client = new OAuth2(
+        '550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',
+        'NiUo_W8rZH3Wa4Ikkr3Ir8by',
+        '',
+    );
+    var scopes = [
+        'https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/calendar'
+    ];
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+    });
+    res.send(url);
+});
+
+app.get('/oauthcallback', function(req, res){
+     req.send(req);
+})
+
+app.listen(port, function(){
+    console.log('--------------'+port);
+});
