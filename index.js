302494fac0774dd42674a268c4336e179d45fbc6
prateek211205361719 <prateek@ceptes.com>
2017-08-22 11:48:55 +0530

    wd3wd3

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    
-    res.send("hello how are u");
+    console.log('----------');
+    var OAuth2 = google.auth.OAuth2;
+    var oauth2Client = new OAuth2(
+        '550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',
+        'NiUo_W8rZH3Wa4Ikkr3Ir8by',
+        '/oauthcallback',
+    );
+    var scopes = [
+        'https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/calendar'
+    ];
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+    });
+    res.send(url);
 });
 
 app.get('/oauthcallback', function(req, res){
