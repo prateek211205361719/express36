114f2dc6138babc82d13124f458b394d1f5f8551
prateek211205361719 <prateek@ceptes.com>
2017-08-22 12:06:49 +0530

    wdwdf33f

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
     console.log('----------');
     var OAuth2 = google.auth.OAuth2;
-    /*var oauth2Client = new OAuth2(
-        '550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',
-        'NiUo_W8rZH3Wa4Ikkr3Ir8by',
-        '/oauthcallback',
-    );
+    var oauth2Client = new OAuth2('550794702620-djqehth8agn6n73kiqdonclmivf5tpe4.apps.googleusercontent.com',  'NiUo_W8rZH3Wa4Ikkr3Ir8by', '/oauthcallback');
     var scopes = [
         'https://www.googleapis.com/auth/plus.me',
         'https://www.googleapis.com/auth/calendar'
@@ -19,7 +15,7 @@ app.get('/', function(req, res){
     var url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: scopes,
-    });*/
+    });
     res.send(OAuth2);
 });
 
